a5b1c7a906fee0b20fe0da57aaae11bc805fd799
MJ <maramjawass@gmail.com>
2026-01-07 13:03:46 +0000

    tried out the exersice given in ch1 ex3.

--- src/Ch01/01_03/CodeDemo.cpp
+++ src/Ch01/01_03/CodeDemo.cpp
@@ -2,13 +2,26 @@
 // Challenge 01_03
 // Console Interaction, by Eduardo Corpeño 
 
-#include <iostream> 
-#include <string>
+#include <iostream> // library for inputs n outputs
+#include <string> // library for strings
 
 int main(){
-    std::string str;
-    std::cin >> str;
-    std::cout << str;
+    // adding in vid request
+    std::cout << "Please enter your name: " ;
+
+
+    std::string str; //declaring a string var and it will hold the user's input
+
+    std::cin >> str; //used in the opposite direction as cout! saying that the input is going into the variable str
+    std::cout << std::endl << std::endl;
+
+    std::cout << "Nice to meet you, " << std::flush; // learned that endl is using std::flush and /n so to get rid of the /n component i used just std::flush
+    
+    std::cout << str; 
+
+    // now taking the input from str and outputting using cout
+
+    // FYI: the cin object can only be used for single words, so for strings with spaces, we have to use a different special function to do so.
 
     std::cout << std::endl << std::endl;
     return (0);
