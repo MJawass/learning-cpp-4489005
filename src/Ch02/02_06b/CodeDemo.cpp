22233e63b6071786e66f690f25904d118eea1a8b
MJ <maramjawass@gmail.com>
2026-01-11 02:31:21 +0000

    finished exercise and lesson ch2_06

--- src/Ch02/02_06b/CodeDemo.cpp
+++ src/Ch02/02_06b/CodeDemo.cpp
@@ -2,10 +2,27 @@
 // Exercise 02_06
 // Preprocessor directives, by Eduardo Corpeño 
 
-#include <iostream>
+#include <iostream> // since it's in angled brackets, the preprocessor will look for the file in a predefined location
+// since iostream is part of the standard library, it doesn't require an extension like http or https
+#include <string>
+#include <cstdint>
+
+#define CAPACITY 5000 // directives do not end with a semicolon
+#define DEBUG
 
 int main(){
-    
+    int32_t large = CAPACITY;
+    uint8_t small = 37;
+
+// directives are not usually indented with the code.
+#ifdef DEBUG
+    std::cout << "[About to perform the addition]" << std::endl;
+#endif
+
+    large += small; // short hand for -> larger = large + small
+    std::cout << "The large integer is " << large << std::endl;
+    // the way to include con conditionally is to use #ifdef directive
+
     std::cout << std::endl << std::endl;
     return (0);
 }
