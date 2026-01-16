32a4beaf768f8c2cd90f69aaa732b123edf4dfab
MJ <maramjawass@gmail.com>
2026-01-16 19:16:50 +0000

    finished lesson and exercise from ch3_06

--- src/Ch03/03_06b/CodeDemo.cpp
+++ src/Ch03/03_06b/CodeDemo.cpp
@@ -6,6 +6,16 @@
 #include <string>
 
 int main(){
+    int a = 37;
+    int *ptr;
+
+    ptr = &a;
+
+    std::cout << a << std::endl << std::endl;
+    std::cout << ptr << std::endl << std::endl;
+    std::cout << &a << std::endl << std::endl;
+    std::cout << *ptr << std::endl << std::endl;
+    std::cout << &ptr << std::endl << std::endl;
 
     std::cout << std::endl << std::endl;
     return (0);
